5af092be688691c7028fa547bc5174061e718b6f
jydzip <dckjeremy.zip@gmail.com>
2024-02-19 14:13:11 +0100

    Add 6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - Start program
+ * Return: (int) Success
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(long int));
+	printf("%lu\n", sizeof(long long int));
+	printf("%lu\n", sizeof(float));
+	return (0);
+}
