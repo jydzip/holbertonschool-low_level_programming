dffdffb2794f2b2f7dfab9fe651ad25cd34c0856
jydzip <dckjeremy.zip@gmail.com>
2024-02-19 16:19:44 +0100

    Fix 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(STDOUT_FILENO, text, sizeof(text));
 	return (1);
