7100701da7850bdaa57f8f509fbe2163b490e089
jydzip <dckjeremy.zip@gmail.com>
2024-02-20 15:28:51 +0100

    Fix 100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -7,25 +7,36 @@
 int main(void)
 {
 	int count = 0;
-	int i = 1;
+	int i = 0;
 	int ii = 0;
 
 	while (count < 90)
 	{
+		i++;
+		count++;
+		if (i == 0 && ii == 0)
+                {
+                        continue;
+                }
+		if (i == ii || ii > i)
+		{
+			continue;
+		}
 		putchar(ii % 10 + '0');
 		putchar(i % 10 + '0');
-		i++;
-		if (i > 9)
+		
+		if (ii == 8 && i == 9)
+                {
+                        break;
+                }
+
+		if (i >= 9)
 		{
 			i = 0;
 			ii++;
 		}
-		if (count < 89)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		count++;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
