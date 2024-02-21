21679ab551b867298029e9da1773cd02849324ab
jydzip <dckjeremy.zip@gmail.com>
2024-02-21 14:01:09 +0100

    Add 2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print all letters of the alphabet, ten times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, count;
+	char *text = "abcdefghijklmnopqrstuvwxyz";
+
+	for (count = 0, count < 10; count++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(text[i]);
+		}
+		_putchar('\n');
+	}
+}
