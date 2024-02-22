f6cae1338376d2f571f71b0d703bc02a473ca796
jydzip <dckjeremy.zip@gmail.com>
2024-02-22 12:12:11 +0100

    Add 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  add - Adds two integers and returns the result
+ *  @a: number one
+ *  @b: number two
+ *
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
