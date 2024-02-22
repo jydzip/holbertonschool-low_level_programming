d727904fbf52959f4c30ddd7eed65ecc8e39cd05
jydzip <dckjeremy.zip@gmail.com>
2024-02-22 10:15:33 +0100

    Fix 8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,22 +7,25 @@
  */
 void jack_bauer(void)
 {
-	int i, ii, iii, iiii;
+	int i, ii, iii, iiii, count;
 
-	for (i = 0; i <= 2; i++)
+	for (count = 0; count < 2; count++)
 	{
-		for (ii = 0; ii <= 3; ii++)
+		for (i = 0; i <= 2; i++)
 		{
-			for (iii = 0; iii <= 5; iii++)
+			for (ii = 0; ii <= 3; ii++)
 			{
-				for (iiii = 0; iiii <= 9; iiii++)
+				for (iii = 0; iii <= 5; iii++)
 				{
-					_putchar(i + '0');
-					_putchar(ii + '0');
-					_putchar(':');
-					_putchar(iii + '0');
-					_putchar(iiii + '0');
-					_putchar('\n');
+					for (iiii = 0; iiii <= 9; iiii++)
+					{
+						_putchar(i + '0');
+						_putchar(ii + '0');
+						_putchar(':');
+						_putchar(iii + '0');
+						_putchar(iiii + '0');
+						_putchar('\n');
+					}
 				}
 			}
 		}
