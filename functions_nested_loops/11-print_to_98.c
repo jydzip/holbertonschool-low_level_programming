636d9a8a3378bdc68cff51fa9f16a722f6708e25
jydzip <dckjeremy.zip@gmail.com>
2024-02-22 15:33:50 +0100

    Add 11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+/**
+ *  print_to_98 - Prints all natural numbers from n to 98
+ *  @n: number max to loop
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i, nb, is_negative, is_increment;
+
+	is_increment = 1;
+	if (n > 98)
+	{
+		is_increment = 0;
+	}
+
+	i = n;
+	while(1 == 1)
+	{
+		is_negative = 0;
+		if (i < 0)
+		{
+			is_negative = 1;
+		}
+	
+		nb = i * ((i > 0) - (i < 0));
+
+		if (is_negative == 1)
+		{
+			_putchar('-');
+		}
+		
+		if ((i >= 0 && i <= 9) || (i >= -9 && i < 0))
+		{
+			_putchar((nb % 10) + '0');
+		}
+		else
+		{
+			if (i > 99)
+			{
+				_putchar((nb / 100) + '0');
+				nb = nb % 100;
+			}
+			_putchar((nb / 10) + '0');
+			_putchar((nb % 10) + '0');
+		}
+
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			break;
+		}
+
+		if (is_increment == 1)
+		{
+			i++;
+		}
+		else
+		{
+			i--;
+		}
+	}
+	_putchar('\n');
+}
