c134502526d061fba0f98cd8f7c80f7e3efa79ac
jydzip <dckjeremy.zip@gmail.com>
2024-02-23 11:00:26 +0100

    Fix 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,8 @@ int print_last_digit(int c)
 {
 	int cc = (int)c;
 	int abs_c = cc * ((cc > 0) - (cc < 0));
-	int last_digit = abs_c % 10;
+	int last_digit = abs_c % 10;	
 
-	_putchar(last_digit + '0');
+	_putchar(-last_digit + '0');
 	return (last_digit);
 }
