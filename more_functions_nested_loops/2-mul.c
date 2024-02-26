c8a69bac98f767ebe6cb2d06df199c654b310661
jydzip <dckjeremy.zip@gmail.com>
2024-02-26 10:19:57 +0100

    Add 2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Miltiplies two integers
+ * @a: (int) character A
+ * @b: (int) character B
+ *
+ * Return: (int) result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
