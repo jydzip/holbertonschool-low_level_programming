d5231c4095228216c2eff2c0a8db240d89c5f45b
jydzip <dckjeremy.zip@gmail.com>
2024-02-26 14:35:26 +0100

    Fix 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 5) == 0)
+		if ((i % 5) == 0 && (i % 3) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
 		}
