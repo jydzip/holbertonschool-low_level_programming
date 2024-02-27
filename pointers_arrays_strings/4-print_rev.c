34c1d62afaf47f8976ee5b7c79d352f73fce92ab
jydzip <dckjeremy.zip@gmail.com>
2024-02-27 14:20:30 +0100

    Add 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+ * print_rev - Prints a string reverse
+ * @s: (char) text
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int length = _strlen(s);
+
+	for (i = length; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - Return length of a string
+ * @s: (int) text
+ * Return: (int) length
+ */
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
