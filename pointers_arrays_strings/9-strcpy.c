8fa3b1fb682367d84b1f115b78d14ecc50453d83
jydzip <dckjeremy.zip@gmail.com>
2024-02-28 10:46:02 +0100

    Fix 9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,10 +9,28 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	int length = _strlen(src);
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < length; i++)
 	{
 		dest[i] = src[i];
 	}
 	return (dest);
+}
+
+/**
+ * _strlen - Return length of a string
+ * @s: (int) text
+ * Return: (int) length
+ */
+int _strlen(char *s)
+{
+        int i;
+        int length = 0;
+
+        for (i = 0; s[i] != '\0'; i++)
+        {
+                length++;
+        }
+        return (length);
 }
