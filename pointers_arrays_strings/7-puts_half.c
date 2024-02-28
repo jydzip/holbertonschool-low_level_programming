1510163d139dcb76631fefbde4a57668c017c6ff
jydzip <dckjeremy.zip@gmail.com>
2024-02-28 09:54:49 +0100

    Add 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+ * puts_half - Prints half of string
+ * @str: (char) text
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int length = _strlen(str);
+
+	for (i = length / 2; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - Return length of a string
+ * @s: (int) text
+ * Return: (int) length
+ */
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
