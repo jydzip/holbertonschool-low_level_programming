970dd2fcfe6855a59353f23636b6a74f500efd4b
jydzip <dckjeremy.zip@gmail.com>
2024-02-29 13:32:45 +0100

    Add 2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * _strncpy - Copies a string
+ * @dest: text one
+ * @src: text two
+ * @n: number of concatenate
+ * Return: (char) result
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - Return length of a string
+ * @s: (int) text
+ * Return: (int) length
+ */
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
