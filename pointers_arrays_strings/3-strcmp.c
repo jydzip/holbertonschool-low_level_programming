32517eaf6a0aca224a3d27f56adfc3b36d9a7c9c
jydzip <dckjeremy.zip@gmail.com>
2024-02-29 14:54:34 +0100

    Add 3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * _strcmp - Compare two string
+ * @s1: text one
+ * @s2: text two
+ * Return: (int)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int r = s1[0] - s2[0];
+
+	return (r);
+}
