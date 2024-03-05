6fb5928a58e9d7747e2d768d18dd202953ec8d7f
jydzip <dckjeremy.zip@gmail.com>
2024-03-05 15:38:07 +0100

    Add 5-

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -11,5 +11,24 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	return (x * _pow_recursion(x, y - 1));
+	return (__sqrt(n, 0));
+}
+
+/**
+ * __sqrt - Operation
+ * @n: size
+ * @i: index
+ * Return: (int) number
+ */
+int __sqrt(int n, int i)
+{
+	if ((i * i) > n)
+	{
+		return (-1);
+	}
+	else if ((i * i) == n)
+	{
+		return (i);
+	}
+	return (__sqrt(n, i + 1));
 }
--- recursion/main.h
+++ recursion/main.h
@@ -16,3 +16,6 @@ int _pow_recursion(int x, int y);
 
 int is_prime_number(int n);
 int check_prime_number(int i, int n);
+
+int _sqrt_recursion(int n);
+int __sqrt(int n, int i);
