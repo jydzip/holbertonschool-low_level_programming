24e400e34c8e81c25252342d9e1eee0e5d91be42
jydzip <dckjeremy.zip@gmail.com>
2024-03-06 10:18:49 +0100

    Fix 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,17 @@
  */
 char *_strchr(char *s, char c)
 {
-	if (c)
-	{
-		while (*s)
+	while (*s)
 		{
-			if (*s == c)
-			{
-				return (s);
-			}
-			s++;
+		if (*s == c)
+		{
+			return (s);
 		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
 	}
 	return (NULL);
 }
