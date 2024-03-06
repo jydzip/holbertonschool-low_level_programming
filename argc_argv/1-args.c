525dc37802daecb8180716d1fa82fe4c78d53920
jydzip <dckjeremy.zip@gmail.com>
2024-03-06 09:24:09 +0100

    Add 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Start program
+ * @argc: Count argument
+ * @argv: Array argument
+ * Return: (int) Success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
