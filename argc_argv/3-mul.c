42bccfd2123973780c7ccd8dfb4d1bd03889bf46
jydzip <dckjeremy.zip@gmail.com>
2024-03-06 09:34:10 +0100

    Add 3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Start program
+ * @argc: Count argument
+ * @argv: Array argument
+ * Return: (int) Success
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	(void)argc;
+
+	if (!argv[1] || !argv[2])
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", a * b);
+	return (0);
+}
