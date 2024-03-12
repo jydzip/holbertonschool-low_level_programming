99322586507ba95f8468400d9e8db238dae4b13b
jydzip <dckjeremy.zip@gmail.com>
2024-03-12 10:33:35 +0100

    Fix 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,15 +13,19 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
+	}
 
 	ptr = malloc((nmemb + 1) * size);
 	if (ptr == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < nmemb + 1; i++)
 	{
-		ptr[i] = '\0';
+		ptr[i] = 0;
 	}
 	return (ptr);
 }
