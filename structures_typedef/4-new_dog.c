c67038e67acfb2ed3c3d04e370eca1d4b1b885f4
jydzip <dckjeremy.zip@gmail.com>
2024-03-13 11:23:56 +0100

    Fix 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include "dog.h"
 
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 /**
  * new_dog - Create a new dog
  * @name: Name of new dog
@@ -13,13 +16,53 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog = malloc(sizeof(dog_t));
-	char *new_name = *name;
-	char *new_owner = *owner;
+	char *new_name = malloc(_strlen(name) + 1);
+	char *new_owner = malloc(_strlen(owner) + 1);
+
+	_strcpy(new_name, name);
+	_strcpy(new_owner, owner);
 
 	if (ndog == NULL)
 		return (NULL);
-	ndog->name = new_name;
+
+	new_name = ndog->name = new_name;
 	ndog->age = age;
 	ndog->owner = new_owner;
 	return (ndog);
+}
+
+/**
+ * _strcpy - Copies the string pointed to by src
+ * @dest: (char) destination
+ * @src: (char) text
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - Return length of a string
+ * @s: (int) text
+ * Return: (int) length
+ */
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
 }
