3688011a05c9c981a45e25c1bd9466cd18a7e87c
jydzip <dckjeremy.zip@gmail.com>
2024-03-13 10:04:41 +0100

    Add dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/**
+ * struct dog - Dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Description: Structure of a dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
