c9fc3f0d7c0737df9c0ea81daf485aac7b4245ab
jydzip <dckjeremy.zip@gmail.com>
2024-03-19 09:46:08 +0100

    Add 0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Sum of all arguments
+ * @n: Number of argument to count
+ *
+ * Return: (int) Result
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int result = 0;
+	unsigned int i;
+	va_list args;
+
+	if (n != 0)
+	{
+		va_start(args, n);
+		for (i = 0; i < n; ++i)
+		{
+			result += va_arg(args, int);
+		}
+		va_end(args);
+	}
+	return (result);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef FUNC_VARIADIC
+#define FUNC_VARIADIC
+int _putchar(char c);
+
+int sum_them_all(const unsigned int n, ...);
+#endif
