e90684003136f5cece47fbcef113fcb917d61afb
jydzip <dckjeremy.zip@gmail.com>
2024-03-20 10:07:01 +0100

    Fix 2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,13 +13,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *sep;
 	va_list args;
 
-	if (n == 0)
-		return;
-	if (separator == NULL)
-		sep = "";
-	else
-		sep = (char *) separator;
-
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
@@ -29,8 +22,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			str = "(nil)";
 
 		printf("%s", str);
-		if (i != (n - 1))
-			printf("%s", sep);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(args);
