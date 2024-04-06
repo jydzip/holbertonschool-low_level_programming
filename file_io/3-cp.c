3999a96cca98f7153d9778988ad4d2ad46e7ceb8
jydzip <dckjeremy.zip@gmail.com>
2024-04-06 17:33:20 +0200

    Fix 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,39 +16,50 @@ int main(int argc, char *argv[])
 	ssize_t bytes, bytes_w;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		_error(97, "");
+
 	filename_from = argv[1];
 	filename_to = argv[2];
 	fd_from = open(filename_from, O_RDONLY);
+
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename_from);
-		exit(98);
-	}
+		_error(98, filename_from);
 	fd_to = open(filename_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
+		_error(99, filename_to);
+
+	bytes = 1;
+	while (bytes)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to);
-		exit(99);
-	}
-	bytes = read(fd_from, buffer, 1024);
-	if (bytes == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename_from);
-		exit(98);
-	}
-	bytes_w = write(fd_to, buffer, bytes);
-	if (bytes_w == -1 || bytes_w != bytes)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to);
-		exit(99);
+		bytes = read(fd_from, buffer, 1024);
+		if (bytes == -1)
+			_error(98, filename_from);
+		if (bytes > 0)
+		{
+			bytes_w = write(fd_to, buffer, bytes);
+			if (bytes_w == -1 || bytes_w != bytes)
+				_error(99, filename_to);
+		}
 	}
 	close_file(fd_from);
 	close_file(fd_to);
 	return (0);
+}
+
+/**
+ * _error - Exit error
+ * @status: Code
+ * @v: Value 1
+ */
+void _error(int status, char *v)
+{
+	if (status == 97)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	else if (status == 98)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", v);
+	else if (status == 99)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", v);
+	exit(status);
 }
 
 /**
