64eb3a6ccecb4d6022ef3911befe028fbe96ce5c
jydzip <dckjeremy.zip@gmail.com>
2024-04-06 17:22:45 +0200

    Fix 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,20 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 
-char *_read_file(char *filename);
-void _create_file(char *filename, char *text_content);
-
 /**
  * main - Start program
  * @argc: Number of arguments
- * @argv: List of arguements
+ * @argv: List of arguments
  * Return: result
  */
 int main(int argc, char *argv[])
 {
+	int fd_from, fd_to;
 	char *filename_from;
 	char *filename_to;
-	char *text_content;
+	char buffer[1024];
+	ssize_t bytes, bytes_w;
 
 	if (argc != 3)
 	{
@@ -23,92 +22,44 @@ int main(int argc, char *argv[])
 	}
 	filename_from = argv[1];
 	filename_to = argv[2];
-
-	text_content = _read_file(filename_from);
-	_create_file(filename_to, text_content);
-	free(text_content);
+	fd_from = open(filename_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename_from);
+		exit(98);
+	}
+	fd_to = open(filename_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to);
+		exit(99);
+	}
+	bytes = read(fd_from, buffer, 1024);
+	if (bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename_from);
+		exit(98);
+	}
+	bytes_w = write(fd_to, buffer, bytes);
+	if (bytes_w == -1 || bytes_w != bytes)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to);
+		exit(99);
+	}
+	close_file(fd_from);
+	close_file(fd_to);
 	return (0);
 }
 
 /**
- * _create_file - Create a file
- * @filename: Filename to create
- * @text_content: Content
+ * close_file - Close file
+ * @fd: File open
  */
-void _create_file(char *filename, char *text_content)
+void close_file(int fd)
 {
-	int fd, fd_value;
-	ssize_t bytes_w;
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd == -1)
+	if (close(fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-
-	if (text_content != NULL)
-	{
-		bytes_w = write(fd, text_content, _strlen(text_content));
-		if (bytes_w == -1)
-		{
-			close(fd);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-			exit(99);
-		}
-	}
-
-	fd_value = close(fd);
-	if (fd_value < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_value);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
-}
-
-/**
- * _read_file - Read a file
- * @filename: Filename to read
- * Return: Content of file
- */
-char *_read_file(char *filename)
-{
-	int fd = open(filename, O_RDONLY);
-	char *buffer;
-	ssize_t bytes;
-
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-
-	buffer = (char *)malloc(sizeof(char) * 1024);
-	bytes = read(fd, buffer, 1024);
-	if (bytes == -1)
-	{
-		close(fd);
-		free(buffer);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	close(fd);
-	return (buffer);
-}
-
-/**
- * _strlen - Return length of a string
- * @s: (int) text
- * Return: (int) length
- */
-int _strlen(char *s)
-{
-	int i;
-	int length = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		length++;
-	}
-	return (length);
 }
