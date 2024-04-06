ce581969e504197790495ae20adf511691f98894
jydzip <dckjeremy.zip@gmail.com>
2024-04-06 13:33:27 +0200

    Fix 7-insert...

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,8 +26,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 	}
 
-	if (node == NULL || node->next == NULL)
+	if (node == NULL)
 		return (NULL);
+	if (node->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -35,9 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = node;
 	new_node->next = node->next;
-
-	if (node->next != NULL)
-		node->next->prev = new_node;
+	node->next->prev = new_node;
 	node->next = new_node;
 	return (new_node);
 }
