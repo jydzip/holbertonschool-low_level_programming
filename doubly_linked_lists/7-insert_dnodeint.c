fb4da4cc6aa5131943dec07647f4fc9f87869482
jydzip <dckjeremy.zip@gmail.com>
2024-04-06 13:26:24 +0200

    Fix 7-insert...

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = node;
 	new_node->next = node->next;
 
-	node->next->prev = new_node;
+	if (node->next != NULL)
+		node->next->prev = new_node;
 	node->next = new_node;
 	return (new_node);
 }
