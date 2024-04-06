b754eb48166a21d7034dbee73bbe495846edc715
jydzip <dckjeremy.zip@gmail.com>
2024-04-06 13:13:47 +0200

    Fix 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,15 +9,14 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	const list_t *node = h;
 
-	while (node != NULL)
+	while (h != NULL)
 	{
-		if (node->str == NULL)
-			printf("[%d] (nil)\n", 0);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", node->len, node->str);
-		node = node->next;
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 		count++;
 	}
 	return (count);
